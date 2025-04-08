814a296f5124f9b82bcc24349fefc9630e25445a
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-04-08 17:00:23 +0600

    day 02 practise code

---
+++ day_02/script.js
@@ -0,0 +1,26 @@
+let name = "mushfiq";
+let age = 27;
+let isStudent = false;
+let favouriteProgrammingLanguage = "javascript";
+console.log(name,age,isStudent, favouriteProgrammingLanguage)
+
+
+
+const myAge = 28;
+
+
+
+ const person = {
+    name : "mushfiiq",
+    age : 27,
+    isStudent : false,
+    favouriteProgrammingLanguage : "javascript"
+ }
+
+ console.log(person)
+
+ let arr = [1,3, 5, 9]
+ console.log(arr)
+
+ arr = [1, 3, 5, 9, 11]
+ console.log(arr)
