dee98c3826324aa80774b2f0491d94994c9b1f61
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-04-09 23:09:37 +0600

    day 03 coding OPERATOR and EXPRESSION

---
+++ day_03/script.js
@@ -0,0 +1,26 @@
+//operator +, -, * , /
+//operands x+y ; here x and y are operands
+//expressions
+
+//Opertars : Arithmetic, assignment, comparison, relational,logical, bitwise, conditionall
+//Grouping: Operator precidence
+//typeOf operator
+//instranceOf operator
+
+let f_name = "mushi";
+let l_name = "cricketer";
+
+console.log(f_name + l_name); //if two varible are string then by + sign they will be concated
+
+// 
+let count = 3;
+console.log(count++); // count = count +1 (first return the value then increses)
+console.log(count)
+
+console.log(++count)// count = count +1; (first increase the value then return)
+
+
+//differenc ebetween == and ===
+
+console.log(0 == false) // true just check the data not data type
+console.log(0 === false)// false strict equality : data and dataype comparison must be same
