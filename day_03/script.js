4745f5def8201da8d7c4f46fcc6f7ffb6b114c75
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-04-11 01:41:10 +0600

    Day 04 - Control flow

--- day_03/script.js
+++ day_03/script.js
@@ -22,5 +22,64 @@ console.log(++count)// count = count +1; (first increase the value then return)
 
 //differenc ebetween == and ===
 
-console.log(0 == false) // true just check the data not data type
+console.log(0 == false) // true losley equality: just check the data not data type
 console.log(0 === false)// false strict equality : data and dataype comparison must be same
+
+//Nan = not a number
+
+let obj1 = {'name': 'mushfiq'}
+let obj2 = {'name': 'mushfiq'}
+
+console.log(obj1 === obj2) //false : memory address are not same
+console.log(obj1 !== obj2)
+
+//It will check from the left
+//if this first operand converted into false it will return first operand otherwise it will give second operand 
+console.log(false && false) // false
+console.log(true && false) // false
+console.log(true && true) // true
+console.log(false && true) // false
+
+//if any other operand is false then the result will be false
+
+console.log("cow" && "horse")// horsse 
+
+//if first operand can be coverted to true it will return first operand otherwise it will return second operand
+console.log(false || false) //false
+console.log(true || false) //true
+console.log(true || true) //true
+console.log(false || true) //true
+
+console.log("cow" || "horse") // cow (in JS any positive value you take like string it can be converted into true)
+
+
+// ??
+//if the first operand or expression can be  converted into null or undefined  return the second otherwise return the second
+//use in database connection
+
+let a1 = null ?? 1 //1
+let a2 = undefined ?? 1 //1
+
+const a3 = false ?? "mushfiq" //false
+const a4 = 0 // "mushfiq" //0
+
+
+//conditional ternary operator
+// condition ? val1 : val2
+let age = 23 ;
+
+
+console.log(age>=60?"senior cetigen" : " non senior citizen")
+
+//Bitwise Operator
+//& | 
+
+// Grouping
+let p =1 
+let q =2
+let r = 3
+console.log(p+q*r)
+
+//typeOf operator return a string which tells the type of oparend
+
+//instanceOf 
---
+++ day_03/task.js
@@ -0,0 +1,50 @@
+//task-01
+let n = 4;
+result = n%2 == 0? "even" : "odd" ;
+console.log(result)
+
+//Task-02
+let age = 27 ;
+
+let license =  age > 18 ? "eligible" : " Not eligible"
+console.log(license)
+
+//task-03
+let monthlySalary = 12300;
+let monthlyBonus = monthlySalary * (0.2);
+
+let annumCTC = (monthlySalary + monthlyBonus ) *12;
+console.log(annumCTC);
+
+//Task -04
+let color = "green";
+
+let trafficComand = (color == "red") ? "Stop" : "Go";
+
+console.log(trafficComand)
+
+//Task - 05
+ let units = 50;
+ let annualElectricityBill = (units *150*365)*0.8 ;
+ console.log(annualElectricityBill)
+
+ //Task-06 
+ let year = 2029;
+ let isleapYear =((year%100!==0) && (year%4==0))? true : false;
+
+ console.log(isleapYear);
+
+ //Task -07
+ let p, q, r;
+ p=4;
+ q=7;
+ r=2;
+
+ let temp = (p>q)?p:q ;
+ let max = temp > r ? temp: r;
+ console.log(max);
+
+ //task -08
+ let count = 5;
+  console.log(count << 1
+  );
